6e81b61bc5e70151a88778ee5d4ae4d5beceb8c6
Kenichiro <saizawa.ks217@gmail.com>
2025-05-01 01:18:34 +0900

    ハンバーガーメニュー修正

--- main.js
+++ main.js
@@ -29,5 +29,6 @@ const hamburger = document.getElementById('hamburger');
 const menu = document.getElementById('menu')
 
 hamburger.addEventListener('click', () => {
-    menu.classList.toggle('active');    
+    hamburger.classList.toggle('active');
+    menu.classList.toggle('active');
 });
